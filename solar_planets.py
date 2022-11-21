9b0c64045ce368fc9a43ef6249429e061779471e
Aneessh-V <87982185+Aneessh-V@users.noreply.github.com>
2022-11-21 18:38:00 +0530

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,12 @@
+import cv2
+cv2.imread(“solar-system.jpg”)
+cv2.imshow(“output”,img).
+cv2.waitKey(0)
+cv2.putText(img
+            "Sun",
+            ('200,300'),
+            cv2.font.HERSHEY.COMPLEX,
+            0.2,
+            (255,255,255),
+            )
+            
